d40d695abac8929b78c105ec6abb8975a3629011
Jusam <717639305@qq.com>
2023-02-05 22:22:38 +0800

    feat: 更新版本至 2022.3

---
+++ src/dotnet/ReSharperPlugin.CodeCompletion/KnownTypes.cs
@@ -0,0 +1,12 @@
+using JetBrains.Metadata.Reader.API;
+using JetBrains.Metadata.Reader.Impl;
+
+namespace ReSharperPlugin.MyAwesomePlugin
+{
+    public static class KnownTypes
+    {
+        public static readonly IClrTypeName ConfigTable = new ClrTypeName("ConfigManager.Table");
+
+        
+    }
+}
